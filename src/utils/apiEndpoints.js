7d51452d09537a7e62dd49e6612fcd246c4ec1e4
datn1007 <datn100705@gmail.com>
2025-08-01 19:58:52 +0700

    edited API_BASE_URL

--- src/utils/apiEndpoints.js
+++ src/utils/apiEndpoints.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'https://smokingcessationsupportswp391-production.up.railway.app/';
+export const API_BASE_URL = 'https://smokingcessationsupportswp391-production-137c.up.railway.app/';
 // export const API_BASE_URL = 'http://localhost:8080';
 export const API_ENDPOINTS = {
   // Authentication
